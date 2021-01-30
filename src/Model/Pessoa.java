378357ddc3a3e9f52fe15f28ee6bac789b2ddc7c
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2021-01-30 16:52:46 -0200

    adiciona model pessoa

--- src/Model/Pessoa.java
+++ src/Model/Pessoa.java
@@ -1,7 +1,11 @@
 
 package Model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 abstract public class Pessoa {
@@ -18,11 +22,15 @@ abstract public class Pessoa {
         this.nome = nome;
     }
 
-    public Pessoa(int id, String nome, char sexo, Date dataNascimento, String telefone, String email, String rg) {
+    public Pessoa(int id, String nome, char sexo, String dataNascimento, String telefone, String email, String rg) {
         this.id = id;
         this.nome = nome;
         this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
+         try {
+            this.dataNascimento = new SimpleDateFormat("dd/MM/yyyy").parse(dataNascimento);
+        } catch (ParseException ex) {
+            Logger.getLogger(Pessoa.class.getName()).log(Level.SEVERE, null, ex);
+        }
         this.telefone = telefone;
         this.email = email;
         this.rg = rg;
