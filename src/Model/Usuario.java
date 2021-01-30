3a7318263ceffd1c1664bf606fe4a3718bde83ed
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2021-01-30 16:42:50 -0200

    utilizando heranca no model usuario

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -3,52 +3,16 @@ package Model;
 
 import java.util.Date;
 
-public class Usuario {
-    private int id;
-    private String nome;
+public class Usuario extends Pessoa{
     private String senha;
     private String nivelAcesso;
-    private char sexo;
-    private Date dataNascimento;
-    private String telefone;
-    private String email;
-    private String rg;
 
-    public Usuario(int id, String nome, String senha, String nivelAcesso, char sexo, Date dataNascimento, String telefone, String email, String rg) {
-        this.id = id;
-        this.nome = nome;
-        this.senha = senha;
-        this.nivelAcesso = nivelAcesso;
-        this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
-        this.telefone = telefone;
-        this.email = email;
-        this.rg = rg;
-    }
-
-    public Usuario(int id, String nome, String senha, String nivelAcesso) {
-        this.id = id;
-        this.nome = nome;
+    public Usuario(String senha, String nivelAcesso, int id, String nome) {
+        super(id, nome);
         this.senha = senha;
         this.nivelAcesso = nivelAcesso;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
+    
     public String getSenha() {
         return senha;
     }
@@ -64,46 +28,5 @@ public class Usuario {
     public void setNivelAcesso(String nivelAcesso) {
         this.nivelAcesso = nivelAcesso;
     }
-
-    public char getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-    
     
 }
